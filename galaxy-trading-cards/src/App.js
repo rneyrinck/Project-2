c82fa8cdf3ee6a5f1a1c135bd47051e72102e425
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-02-11 18:16:26 -0600

    figured out logic and function for grabbing random page from API -> App.js line 14 randomPage. Then added that to App.js line 35 getGalaxyAPI for url

--- galaxy-trading-cards/src/App.js
+++ galaxy-trading-cards/src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './App.css';
 import Footer from './footer/footer';
 import GalaxyCard from './galaxyCard/galaxyCard';
@@ -8,11 +9,42 @@ import Main from './main/main';
 
 function App() {
   const urlGalaxy = 'https://images-api.nasa.gov/search?q=galaxy'
+  // need random formula for which page to send the url to out of 18 total pages
+  // example of 18th page -> https://images-api.nasa.gov/search?q=galaxy&page=18
+  const randomPage = () => {
+    // resource for MATH https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    let min = Math.ceil(1)
+    let max = Math.floor(19)
+    let pageMath = Math.floor(Math.random()*(max-min)+min)
+    let pageUrl = `&page=${pageMath}`  
+    console.log(pageUrl)
+    return(
+     pageUrl
+    )
+  }
+  randomPage()
+ 
+  // set state for GalaxyCard info
+  const [galaxyList,setGalaxyList] = useState([])
+  // function for random selection of galaxy items after api call
+  const randomGalaxy = () => {
+    // 
+  }
+
   // create function that uses fetch to call the nasa api
   const getGalaxyAPI = () => {
-    // const url = urlGalaxy  
-    // fetch(url)
+    const url = `${urlGalaxy}${randomPage}` 
+    fetch(url)
+    .then((res)=>res.json())
+    // \/\/\/\\/set carddata to be the retunr without any metadata, version info, or other unused information
+    .then((data)=>setGalaxyList(data.items))
+    .catch(()=>console.log('issue in getGalaxyAPI, App.js line 20'))
   }
+
+  // take that getgalaxyAPI functin and send it to the button component
+  // onclick in button component activate that fetch function
+  // send it back up the tree to APP then back to galaxy card to populate name and image
+  // because there will only be one component at a time we only need one state 
   return (
     <div className="App">
      <Header />
