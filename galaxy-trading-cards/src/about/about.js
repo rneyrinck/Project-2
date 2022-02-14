8448f1eddf175c5d4b3a9166024114b68da61f8b
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-02-14 11:06:18 -0600

    got stuck on the initial api request, gave it some room to breath and did some css styling

--- galaxy-trading-cards/src/about/about.js
+++ galaxy-trading-cards/src/about/about.js
@@ -1,30 +1,32 @@
 import "./about.css";
+
 const About = () => {
   return (
     <section className="about-card">
-      <div>
+      <div className="main-text">
         <h1>About</h1>
         <p>
           App for randomly generating a trading card with an image of a galaxy,
           that galaxies name, and in future versions, giving information about
           that galaxy and allowing the user to collect, download and trade those
           cards.
-          <br />
+
           Implementing NASA's image library API, with the keyword 'Galaxy', this
           App returns a randomly selected galaxy card from 1774 different
           options in the API. 
           <br />
+          
           The MVP will be a proof of concept limiting the scope to a galaxy card
           generator with an about page describing the app.
-          <br />
+          
           Upon future iterations new features will be added, such as a card
           click feature where when a user generates a card they can flip it to
           read more information on that card.
         </p>
       </div>
-      <div className="img-stuff">
-        <img src="" alt="pic-o-bob" />
-        <div className="img-stuff-name">
+      <div className="img-hero">
+        <img src="https://rneyrinck.github.io/Portfolio/me3.png" alt="pic-o-bob" />
+        <div className="img-text">
         <h6>Robert Neyrinck</h6>
         <p>Software Engineer</p>
         </div>
